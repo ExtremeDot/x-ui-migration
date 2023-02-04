31ba5ad00df27951ca987fad151ab222c4fc0a66
LostInPersia <120102306+ExtremeDot@users.noreply.github.com>
2023-02-04 13:50:12 +0330

    Update XUI-MIG.sh

--- XUI-MIG.sh
+++ XUI-MIG.sh
@@ -66,6 +66,7 @@ green "Welcome to ExtremeDot X-UI panel migration Script"
 echo
 
 function oLDInfo() {
+clear
 green "Please Enter OLD SERVER information here"
 echo
 read -e -i "$OLD_IPv4" -p "OLD SERVER: Please input PUBLIC IP v4: " input
@@ -99,6 +100,7 @@ fi; fi
 
 ### NEW SERVER DATA
 function nEWInfo() {
+clear
 echo "Please Enter NEW SERVER information here"
 echo
 read -e -i "$NEW_IPv4" -p "NEW SERVER: Please input PUBLIC IP v4: " input
@@ -130,21 +132,109 @@ fi; fi
 }
 
 ### DOMAIN CHECK
-#DOMAIN_ADDRESS=
-#EMAIL_ADDRESS=
-function dOMAINiNFO() {
+function gETdOMAINiNFO() {
 clear
-echo; green "New Server Domain Check"; echo
-if [[ -z $DOMAIN_ADDRESS ]]; then #DOMAIN ADDRESS IS NOT ENTERED
-red "NEW SERVER: ERROR getting DOMAIN name"; echo
+echo; red "NEW SERVER: ERROR getting DOMAIN name"; echo
 read -e -i "$DOMAIN_ADDRESS" -p "NEW SERVER: Please enter damain address: " input
 DOMAIN_ADDRESS="${input:-$DOMAIN_ADDRESS}"
-fi
+cHECKdOMAINiNFO
+}
 
+function cHECKdOMAINiNFO() {
+green "New Server Domain Check"; echo
+if [[ -z $DOMAIN_ADDRESS ]]; then #DOMAIN ADDRESS IS NOT ENTERED
+gETdOMAINiNFO
+else
+echo ; yellow "DomainName= [$DOMAIN_ADDRESS]";
+DOMAINANMES=""
+until [[ $DOMAINANMES =~ (y|n) ]]; do
+read -rp "NEW SERVER: Confirm Domain Name? [y/n]: " -e -i y DOMAINANMES
+done
+if [[ $DOMAINANMES == "n" ]]; then
+yellow "Specify Domain Name"
+gETdOMAINiNFO
+fi; fi
+}
 
+######## EMAIL CHECK
+#EMAIL_ADDRESS=
+function gETeMAILiNFO() {
+clear
+echo; red "NEW SERVER: ERROR getting E-MAIL Address"; echo
+read -e -i "$EMAIL_ADDRESS" -p "NEW SERVER: Please enter E-MAIL address: " input
+EMAIL_ADDRESS="${input:-$EMAIL_ADDRESS}"
+cHECKeMAILiNFO
+}
 
+function cHECKeMAILiNFO() {
+green "New Server Email Address Check"; echo
+if [[ -z $EMAIL_ADDRESS ]]; then #E-MAIL ADDRESS IS NOT ENTERED
+gETeMAILiNFO
+else
+echo ; yellow "E-MAIL Address= [$EMAIL_ADDRESS]";
+EMAILANMES=""
+until [[ $EMAILANMES =~ (y|n) ]]; do
+read -rp "NEW SERVER: Confirm Domain Name? [y/n]: " -e -i y EMAILANMES
+done
+if [[ $EMAILANMES == "n" ]]; then
+yellow "Specify E-MAIL ADDRESS"
+gETeMAILiNFO
+fi; fi
+}
 
+function cHECKdOMAINiP() {
+echo
+green "$DOMAIN_ADDRESS IP must be $NEW_IPv4, now check the ip of domain!"
+IPCHECKNEWDOMAIN=`dig +short $DOMAIN_ADDRESS` && sleep 1
+DOMCHECKS=""
+until [[ $DOMCHECKS =~ (y|n) ]]; do
+read -rp "[$IPCHECKNEWDOMAIN] is set to [$DOMAIN_ADDRESS], is Correct? [y/n]: " -e -i y DOMCHECKS
+done
+if [[ $DOMCHECKS == "n" ]]; then
+red "Setup proccess is paused now"
+yellow "goto Cloudflare dashboard and set [$NEW_IPv4] IP for [$DOMAIN_ADDRESS]"
+yellow "don't forget to Set to \"DNS ONLY\" "
+echo
+green "when you change the domain to new ip , press Enter to continue"
+DOMCHECKS2=""
+until [[ $DOMCHECKS2 =~ (y|n) ]]; do
+read -rp "Have you updated Domain Settings to New Server's IP? [y/n]: " -e -i y DOMCHECKS2
+done
+if [[ $DOMCHECKS2 == "n" ]]; then
+cHECKdOMAINiP
+fi; fi
+}
+
+function cHECKoVERALLiNFO() {
+clear
+green "V2RAY X-UI PANEL SERVER CHANGER  V2.0"; echo
+green "V2RAY PANEL: DOMAIN=[$DOMAIN_ADDRESS] - E-MAIL=[$EMAIL_ADDRESS]"
+green "OLD SERVER: IP=[$OLD_IPv4] - USER=[$OLD_LOGINNAME] - PASS=[$OLD_PASSWORD]"
+yellow "NEW SERVER: IP=[$NEW_IPv4] - USER=[$NEW_LOGINNAME] - PASS=[$NEW_PASSWORD]"
+green "NEW PANEL [$DOMAIN_ADDRESS] is set to [$IPCHECKNEWDOMAIN]"
+echo
+OVERALLCHECK=""
+until [[ $OVERALLCHECK =~ (y|n) ]]; do
+read -rp "Everything is ok and Start moving OLD Server to New? [y/n]: " -e -i y OVERALLCHECK
+done
+sTARTmIGRATION
+}
+
+function sTARTmIGRATION() {
+clear
+echo ; green "Start Moving X-UI Panel to New Server"
+
+}
+
+echo ; blue "-OLD SERVER==================="; echo
 oLDgETiNFO
-echo ; blue "------------------"; echo
+echo ; blue "-NEW SERVER==================="; echo
 nEWgETiNFO
-
+echo ; blue "-DOMAIN CHECK================="; echo
+cHECKdOMAINiNFO
+echo ; blue "-EMAIL CHECK=================="; echo
+cHECKeMAILiNFO
+echo ; blue "-DOMAIN NEW IP CHECK=========="; echo
+cHECKdOMAINiP
+echo ; blue "-OVERALL CHECK================"; echo
+cHECKoVERALLiNFO
